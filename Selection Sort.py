0627161a7b7314e9a9c572459f06b80e3fe48dc7
Perumal215 <ssirangjivi@gmail.com>
2025-09-25 18:56:36 +0530

    mo-selection sort

--- Selection Sort.py
+++ Selection Sort.py
@@ -22,3 +22,12 @@ print("Unsorted array:", arr)
 selection_sort(arr)
 
 print("Sorted array:", arr)
+N = int(input())   # 1
+arr = [input().strip() for _ in range(N)]   # 2
+
+for i in range(N):   # 3
+    niceness = 0     # 4
+    for j in range(i):   # 5
+        if arr[j] < arr[i]:   # 6
+            niceness += 1     # 7
+    print(niceness)   # 8
