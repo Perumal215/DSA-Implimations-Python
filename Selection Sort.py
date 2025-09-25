1f1c1affccf4cd90772dd0d98510633c1a8b86a8
Perumal215 <ssirangjivi@gmail.com>
2025-09-25 19:14:31 +0530

    selection sort

---
+++ Selection Sort.py
@@ -0,0 +1,24 @@
+
+
+def selection_sort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        # Assume the minimum is the first element
+        min_index = i
+        
+        # Find the index of the smallest element in the remaining array
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        
+        # Swap the found minimum element with the first element of the unsorted part
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+
+
+arr = [64, 25, 12, 22, 11]
+print("Unsorted array:", arr)
+
+selection_sort(arr)
+
+print("Sorted array:", arr)
